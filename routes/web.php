9ecaf45f62c03cfc02ff04c50cbef193d8247b6f
baihaqii238 <arknightarknightoo@gmail.com>
2024-05-05 14:34:00 +0700

    ubah

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 // Route untuk indeks langsung
-Route::resource('\posts', \App\Http\Controllers\PostController::class);
+Route::get('/', [\App\Http\Controllers\PostController::class, 'index']);
 
-
+// Route resource
+Route::resource('posts', \App\Http\Controllers\PostController::class);
