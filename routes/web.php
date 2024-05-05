5c3731f4125508fc132a85f311b212c75f15b866
baihaqii238 <arknightarknightoo@gmail.com>
2024-05-05 14:39:44 +0700

    ewf

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route untuk indeks langsung
-Route::get('/posts/index', [\App\Http\Controllers\PostController::class, 'index']);
-
-// Route resource
-Route::resource('posts', \App\Http\Controllers\PostController::class);
+//route resource
+Route::resource('/', \App\Http\Controllers\PostController::class);
