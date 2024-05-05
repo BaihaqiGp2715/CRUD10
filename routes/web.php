12017a9ff2186bcc3919723ab7a343bbffb3bcb4
baihaqii238 <arknightarknightoo@gmail.com>
2024-05-05 14:47:54 +0700

    wqdqw

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
 
-use Illuminate\Http\Request;
-use App\Models\Post;
-
-class PostController extends Controller
-{
-    // Method untuk menampilkan halaman index
-    public function index()
-    {
-        // Ambil semua data posts dari database
-        $posts = Post::all();
-
-        // Tampilkan halaman index.blade.php dan lewatkan data posts ke dalam view
-        return view('posts.index', compact('posts'));
-    }
-
-    // Metode lain seperti store(), create(), edit(), update(), dan destroy() tetap tidak berubah
-}
+//route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
